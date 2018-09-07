b590488c3dbaf8dbaeb2f16925e21202e1edd6de
deepexi <2719891907@qq.com>
2018-09-08 00:47:34 +0800

    更新文件 start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 git pull
 yarn --registry=https://registry.npm.taobao.org/
 yarn build
-rm -fr /usr/share/nginx/html/moby-platform-dashboard
-mv dist /usr/share/nginx/html/moby-platform-dashboard
+rm -fr /usr/share/nginx/html/yunding-platform-dashboard
+mv dist /usr/share/nginx/html/yunding-platform-dashboard
