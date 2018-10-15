c3d2b2d2c274c0944b86a8bf33732e56bc9c731c
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2018-10-15 16:11:13 +0800

    【订单详情】add 订单详情页面

--- src/pages/order/order-detail.vue
+++ src/pages/order/order-detail.vue
@@ -1,309 +1,197 @@
-<!--
- /*
- * @Author: wukunpeng 
- * @Date: 2018-08-25 15:53:14 
- * @Last Modified by: wukunpeng
- * @Last Modified time: 2018-08-30 15:12:08
- * 新车订单详情页面
- */
- -->
 <template>
-  <div class="order-detail-car">
+  <div class="order-detail">
     <el-card shadow="never"
              class="border-0">
       <div slot="header">
-        <h2>订单详情</h2>
+        <go-back></go-back>
       </div>
-      <card-table header="订单信息">
+      <card-table header="订单基本信息">
         <table-info :table="orderInfoTable"></table-info>
       </card-table>
-      <card-table header="车辆信息">
-        <div class="car-info">
-          <div class="img">
-            <img :src="this.car.imgUrl" />
-          </div>
-          <div class="table">
-            <table-info :table="carInfoTable"></table-info>
-          </div>
-        </div>
+
+      <card-table header="收货信息">
+        <table-info :table="deliveryInfoTable"></table-info>
       </card-table>
-      <card-table header="支付信息"
-                  v-if="payment.length>0">
-        <el-table :data="payment"
+
+      <card-table header="订单商品信息">
+        <el-table :data="skuList"
                   border
                   style="width: 100%">
-          <el-table-column prop="tradeOrderId"
-                           label="支付流水号">
+          <el-table-column prop="skuId"
+                           min-width='120'
+                           :show-overflow-tooltip="true"
+                           label="商品编码">
           </el-table-column>
-          <el-table-column prop="payMoney"
+          <el-table-column prop="skuName"
+                           min-width='120'
+                           :show-overflow-tooltip="true"
+                           label="商品名称">
+          </el-table-column>
+          <el-table-column prop="sellPrice"
+                           min-width='120'
+                           label="商品单价（元）">
+          </el-table-column>
+          <el-table-column prop="quantity"
+                           min-width='80'
+                           label="购买数量">
+          </el-table-column>
+          <el-table-column prop="itemMoney"
+                           min-width='100'
+                           label="合计金额">
+          </el-table-column>
+          <el-table-column prop="discountMoney"
+                           min-width='100'
+                           label="优惠金额">
+          </el-table-column>
+          <el-table-column prop="integrationMoney"
+                           min-width='160'
+                           label="积分金额（分/金额）">
+          </el-table-column>
+          <el-table-column prop="couponMoney"
+                           min-width='160'
+                           label="优惠券（名称/金额）">
+          </el-table-column>
+        </el-table>
+      </card-table>
+
+      <!-- <card-table header="订单发票信息">
+      </card-table> -->
+
+      <card-table header="订单支付信息"
+                  v-if="payList.length>0">
+        <el-row  class="row-bg">
+          <el-col :span="12" style="margin-bottom: 20px">订单编号: {{detail.orderCode}}</el-col>
+          <el-col :span="12" style="margin-bottom: 20px">源通币支付: {{detail.currencyPay}}</el-col>
+          <el-col :span="12" style="margin-bottom: 20px">现金支付: {{detail.crashPay}}</el-col>
+        </el-row>
+        <el-table :data="payList"
+                  border
+                  style="width: 100%;">
+          <el-table-column prop="payType"
+                           label="支付方式">
+          </el-table-column>
+          <el-table-column prop="payStreamId"
                            width='80'
-                           label="支付金额">
+                           label="支付流水">
           </el-table-column>
           <el-table-column prop="payChannel"
                            width='80'
                            label="支付通道">
           </el-table-column>
-          <el-table-column prop="payStatus"
+          <el-table-column prop="payMoney"
                            width='80'
-                           label="支付状态">
+                           label="支付金额">
           </el-table-column>
-          <el-table-column prop="paidAt"
+          <el-table-column prop="payDateStr"
                            width='180'
-                           :formatter="row=>formatDate(row.paidAt)"
                            label="支付时间">
           </el-table-column>
-
         </el-table>
       </card-table>
-
-      <div class="tab-container">
-        <el-tabs v-model="activeTab">
-          <el-tab-pane label="贷款信息"
-                       name="1">
-            <table-info :table="installmentInfoTable"></table-info>
-          </el-tab-pane>
-          <el-tab-pane label="保险信息"
-                       name="2">
-            <table-info :table="insuranceInfoTable"></table-info>
-          </el-tab-pane>
-          <el-tab-pane label="精品信息"
-                       name="3">
-            <el-table :data="boutiqueInfoTable"
-                      border
-                      style="width: 100%">
-              <el-table-column prop="itemName"
-                               label="商品名称"
-                               width="180">
-              </el-table-column>
-              <el-table-column prop="quantity"
-                               label="数量"
-                               width="180">
-              </el-table-column>
-              <el-table-column prop="itemMoney"
-                               label="价格">
-              </el-table-column>
-            </el-table>
-          </el-tab-pane>
-          <el-tab-pane label="服务信息"
-                       name="4">
-            <table-info :table="serviceInfoTable"></table-info>
-          </el-tab-pane>
-          <el-tab-pane label="订单资料"
-                       name="5">
-            <div v-if="detail.images">
-              <img  style="width:200px;margin-right:10px" :src="item.url" :alt="item.type" v-for="item in (detail.images)" :key="item.id">
-            </div>
-            <div class="text-center"
-                 v-else>
-              <p style="color:#909399;padding:20px">暂无数据</p>
-            </div>
-          </el-tab-pane>
-        </el-tabs>
-      </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import GoBack from '@/components/GoBack'
 import CardTable from '@/components/card-table'
 import TableInfo from '@/components/table-info'
 import {orderDetail} from '@/const/api'
 import {formatDate, Object2Options, toOptionsLabel, price} from '@/const/filter'
-import {orderStatusOptions, orderTypeOptions, productType} from '@/const/config'
-import zipObject from 'lodash/zipObject'
-import zipWith from 'lodash/zipWith'
-import split from 'lodash/split'
+
+const itemSource = {
+  '0': '我买网',
+  '1': '自营'
+}
 export default {
   name: 'order-detail-car',
   components: {
     CardTable,
-    TableInfo
+    TableInfo,
+    'go-back': GoBack
   },
   data() {
     return {
-      activeTab: '1',
       detail: {} //订单详情
     }
   },
   methods: {
-    formatterOrderStatus(status) {
-      return toOptionsLabel(status, orderStatusOptions)
-    },
-    formatDate
+    // formatterOrderStatus(status) {
+    //   return toOptionsLabel(status, orderStatusOptions)
+    // },
   },
   computed: {
     orderId() {
       return this.$route.query.id
     },
-    products() {
+    skuList() {
       //商品，多个，是个数组
-      return this.detail.outDtoTradeOrderLines || []
+      return this.detail.skuList || []
     },
-    car() {
-      //新车
-      const data = this.products.filter(
-        item => item.productType === productType['新车']
-      )
-      return data[0] || {}
-    },
-    installment() {
-      //分期
-      const data = this.products.filter(
-        item => item.productType === productType['分期']
-      )
-      return data[0] || {}
-    },
-    insurance() {
-      //保险
-      const data = this.products.filter(
-        item => item.productType === productType['保险']
-      )
-      return data[0] || {}
-    },
-    boutique() {
-      //精品
-      const data = this.products.filter(
-        item => item.productType === productType['精品']
-      )
-      return data[0] || {}
-    },
-    service() {
-      //服务
-      const data = this.products.filter(
-        item => item.productType === productType['服务']
-      )
-      return data[0] || {}
-    },
-    trade() {
-      //物流，是个对象
-      return this.detail.outDtoTradeOrder || {}
-    },
-    payment() {
+    payList() {
       //支付信息，是个数组
-      return this.detail.outDtoOrderPayments || []
+      return this.detail.payList || []
     },
     orderInfoTable() {
       //订单信息
-      //Todo:定金,客户类型,订单状态
       const {
-        id,
-        createdAt,
-        memberName,
-        memberType,
-        memberPhone,
-        shopName,
-        remark,
+        orderCode,
+        itemMoney,
+        source, // 0 我买网 1自营
+        orderDateStr,
         status,
-        payMoney = '',
-        maxDeposit = ''
-      } = this.trade
+        payDateStr,
+        user,
+        remark
+      } = this.detail
       const data = {
-        创建时间: formatDate(createdAt),
-        订单号: id,
-        手机号: memberPhone,
-        客户姓名: memberName,
-        // 客户类型: memberType, //30号演示版本去掉
-        门店: shopName,
-        订单金额: price(payMoney),
-        定金: price(maxDeposit),
-        订单状态: toOptionsLabel(status, orderStatusOptions),
-        客户备注: remark
+        订单编号: orderCode,
+        订单总金额: price(itemMoney),
+        商品来源: itemSource[source],
+        下单时间: orderDateStr,
+        支付状态: status,
+        支付时间: payDateStr,
+        下单账号: user,
+        下单备注: remark
       }
       return Object2Options(data, 'value')
     },
-    carInfoTable() {
-      //车辆状态 0=不可交付, 1=可交付, 2=已交付(订单完成)
-      const config = {
-        0: '不可交付',
-        1: '可交付',
-        2: '已交付'
-      }
-      const data = {
-        车系: this.car.bundleCarSeriesName,
-        品牌: this.car.bundleCarBrandName,
-        车型编号: this.car.vehicleModelCode,
-        车型名称: this.car.bundleCarModelName,
-        指导价: price(this.car.guidancePrice),
-        门店单价: price(this.car.hopPrice),
-        规格组合: this.car.propValues,
-        车架号: this.car.vehicleVinCode,
-        物流状态: config[this.car.vehicleDeliver]
-      }
-      return Object2Options(data, 'value')
-    },
-    installmentInfoTable() {
-      //贷款信息
-      const installmentInfo = this.installment.lines
-        ? this.installment.lines[0]
-        : {}
+    deliveryInfoTable() {
       const {
-        itemName,
-        propNames,
-        propValues,
-        loadAmount,
-        servicePrice,
-        responsiblePeopleName
-      } = installmentInfo
-
-      const props = zipObject(split(propNames, ','), split(propValues, ','))
-
-      const data = {
-        商品名称: itemName,
-        首付比例: props['首付比例'],
-        期数: props['期数'],
-        首付金额: price(props['首付']),
-        贷款额: price(loadAmount),
-        月供: price(props['月供']),
-        手续费: price(servicePrice),
-        金融经理: responsiblePeopleName
-      }
-      return Object2Options(data, 'value')
-    },
-    insuranceInfoTable() {
-      //保险信息
-      const insuranceInfo = this.insurance.lines ? this.insurance.lines[0] : {}
-      const {
-        itemName,
-        propNames,
-        propValues,
-        itemMoney = '',
-        responsiblePeopleName
-      } = insuranceInfo
-
-      const props = zipWith(
-        split(propNames, ','),
-        split(propValues, ','),
-        function(a, b) {
-          return `${a}:${b}`
-        }
-      )
+        receiverName,
+        receiverEmail,
+        receiverMobile,
+        province,
+        city,
+        area,
+        address
+      } = this.detail
 
       const data = {
-        商品名称: itemName,
-        套餐内容: props.join(';'),
-        商品金额: price(itemMoney),
-        承保顾问: responsiblePeopleName
-      }
-      return Object2Options(data, 'value')
-    },
-    boutiqueInfoTable() {
-      //精品信息
-      return this.boutique.lines || []
-    },
-    serviceInfoTable() {
-      //服务信息
-      const data = {
-        商品名称: this.service.bundleName,
-        套餐内容: this.service.bundleContent,
-        商品金额: price(this.service.bundlePrice),
-        验车专员: this.service.responsiblePeopleName
+        收货人: receiverName,
+        收货人邮箱: receiverEmail,
+        收货人电话: receiverMobile,
+        '省/直辖市': province,
+        '市/直辖市': city,
+        '区/县': area,
+        详细地址: address
       }
       return Object2Options(data, 'value')
     }
   },
   mounted() {
-    this.$axios.$get(`${orderDetail}?id=${this.orderId}`).then(result => {
+    this.$axios.$get(`${orderDetail}?orderId=${this.orderId}`).then(result => {
       this.detail = result.payload
+
+      // this.detail.payList = [
+      //   {
+      //     payType: '', //支付方式
+      //     payStreamId: '', //支付流水id
+      //     payChannel: '', //支付通道
+      //     payMoney: '', //支付金额
+      //     payDateStr: '' //支付时间
+      //   }
+      // ]
     })
   }
 }
