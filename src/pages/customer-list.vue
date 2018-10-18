4bf0bcd4d576a4ac242d885c0f22be4c107b04a4
Edgar <15989069487@163.com>
2018-10-18 18:33:56 +0800

    客户列表-单个充值弹窗样式

--- src/pages/customer-list.vue
+++ src/pages/customer-list.vue
@@ -1,7 +1,49 @@
 <template>
   <div :class="pageName">
-    <el-data-table ref="dataTable" v-loading="loading" :url="url" :columns="columns" :hasNew="false" :hasEdit="false" :hasDelete="false" :hasOperation="true" :isTree="false" :hasPagination="true" :extraButtons="extraButtons" :searchForm="searchForm" :dataPath="dataPath" :totalPath="totalPath">
+    <el-data-table
+      ref="dataTable"
+      v-loading="loading"
+      :url="url"
+      :columns="columns"
+      :hasNew="false"
+      :hasEdit="false"
+      :hasDelete="false"
+      :hasOperation="true"
+      :isTree="false"
+      :hasPagination="true"
+      :extraButtons="extraButtons"
+      :operationAttrs="operationAttrs"
+      :searchForm="searchForm"
+      :dataPath="dataPath"
+      :totalPath="totalPath">
     </el-data-table>
+
+    <el-dialog :title="topUpDialogTitle" :visible.sync="topUpDialogVisible">
+      <el-form ref="topUpform" label-width="100px" label-position="right" :model="topUpform" :rules="topUpRules">
+        <template v-if="currentDialog === single">
+          <el-form-item label="昵称">
+            {{topUpform.nickName}}
+          </el-form-item>
+          <el-form-item label="手机号">
+            {{topUpform.mobile}}
+          </el-form-item>
+          <el-form-item label="当前源通币">
+            {{topUpform.coin || 0}}
+          </el-form-item>
+          <el-form-item label="充值数量" prop="count">
+            <el-input v-model.trim="topUpform.count"></el-input>
+          </el-form-item>
+        </template>
+        <template v-else>
+
+        </template>
+
+        <el-form-item>
+          <el-button type="primary" @click="topUp">保存</el-button>
+          <el-button @click="topUpDialogVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -10,14 +52,27 @@ import {formatDate} from '@/const/filter'
 import {mcMemberInfos, getShopUserInfo} from '@/const/api'
 import {customerDetail} from '@/const/path'
 
+const dialogTitle = {
+  batch: '批量充值',
+  single: '源通币充值'
+}
+const single = 'single'
+const batch = 'batch'
 export default {
   name: 'customer-list',
   data() {
+    const validateCount = (rule, value, callback) => {
+      if (!value) {
+        callback('请输入数量')
+      } else {
+        callback()
+      }
+    }
     return {
       pageName: 'customer-list',
       url: getShopUserInfo, //总部端分页查询
-      totalPath: 'payload.totalElements',
-      dataPath: 'payload.content',
+      totalPath: 'payload.total',
+      dataPath: 'payload.list',
       columns: [
         {
           prop: 'nickName',
@@ -55,23 +110,19 @@ export default {
         }
       ],
       loading: false,
+      operationAttrs: {
+        width: '200px'
+      },
       extraButtons: [
         {
           text: '源通币充值',
-          // show: row => row.status === 2,
           type: 'primary',
-          atClick: this.toggleStatus
+          atClick: this.showTopUp
         },
         {
           text: '查看',
           atClick: this.go2Detail
         }
-        // {
-        //   text: '禁用',
-        //   show: row => row.status === 1,
-        //   type: 'danger',
-        //   atClick: this.toggleStatus
-        // }
       ],
       searchForm: [
         {
@@ -90,28 +141,44 @@ export default {
           $id: 'mobile',
           $type: 'input'
         }
-      ]
+      ],
+
+      single,
+      batch,
+      topUpform: {
+        count: 0
+      },
+      topUpRules: {
+        count: {trigger: 'blur', validator: validateCount}
+      },
+      currentDialog: single,
+      topUpDialogTitle: dialogTitle[this.currentDialog],
+      topUpDialogVisible: false
+    }
+  },
+  watch: {
+    topUpDialogVisible: function(isShow) {
+      if (!isShow) {
+        this.topUpform = {}
+        this.$refs.topUpform.resetFields()
+      }
     }
   },
   methods: {
     go2Detail(row) {
       this.$router.push(`${customerDetail}?memberId=${row.id}`)
     },
-    toggleStatus({id, status}) {
-      // this.loading = true
-      // this.$axios
-      //   .$put(`${mcMemberInfos}/${id}`, {
-      //     // status: status === 1 ? 2 : 1
-      //     status: 3 - status
-      //   })
-      //   .then(() => {
-      //     this.loading = false
-      //     this.$refs.dataTable.getList()
-      //   })
-      //   .catch(e => {
-      //     this.loading = false
-      //     console.log(e)
-      //   })
+    topUp() {
+      this.$refs.topUpform.validate(valid => {
+        if (valid) {
+          this.topUpDialogVisible = false
+        }
+      })
+    },
+    showTopUp(row) {
+      this.currentDialog = single
+      this.topUpDialogVisible = true
+      this.topUpform = row
     }
   }
 }
