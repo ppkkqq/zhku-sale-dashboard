53d2f6c1bf0027a3b01c68f24e7985259f047b4f
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2018-10-19 15:33:57 +0800

    add 商品审核列表、详情

---
+++ src/pages/goods/goods-audit-detail.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="goods-detail">
+    <el-card shadow="never"
+             class="border-0">
+      <div slot="header">
+        <h2>{{data.name}}</h2>
+      </div>
+      <card-table header="基础信息">
+        <table-info style="width:700px">
+          <tr>
+            <td class="label">后台类目</td>
+            <td class="value"
+                colspan="3">{{catalogName}}</td>
+          </tr>
+          <tr>
+            <td class="label">品牌</td>
+            <td class="value"
+                colspan="3">{{brandName}}</td>
+          </tr>
+          <tr>
+            <td class="label">定金</td>
+            <td class="value"
+                colspan="3">{{price(data.depositPercent || 0)}}</td>
+          </tr>
+          <tr>
+            <td class="label">广告语</td>
+            <td class="value"
+                colspan="3">{{data.title}}</td>
+          </tr>
+          <tr>
+            <td class="label">商品图片</td>
+            <td class="value"
+                colspan="3">
+              <img :src="item"
+                   v-for="(item,index) in productPhoto"
+                   :key="index" />
+            </td>
+          </tr>
+        </table-info>
+      </card-table>
+      <card-table header="模板属性">
+        <el-table :data="data.itemAttributes || []"
+                  border>
+          <el-table-column prop="groupName"
+                           label="属性组"
+                           width="100px">
+          </el-table-column>
+          <el-table-column prop="propName"
+                           label="属性名">
+          </el-table-column>
+          <el-table-column prop="propValue"
+                           label="属性值">
+          </el-table-column>
+          <el-table-column prop="type"
+                           :formatter="formatter"
+                           label="属性类型">
+          </el-table-column>
+        </el-table>
+      </card-table>
+      <card-table header="规格组合"
+                  v-if=" skuTableColumn.length > 0">
+        <el-table :data="skuTableData || []"
+                  border>
+          <el-table-column v-for="(item,index) in skuTableColumn"
+                           :prop="item.prop"
+                           :label="item.label"
+                           :key="index">
+          </el-table-column>
+
+        </el-table>
+      </card-table>
+      <card-table header="配置方案">
+        <el-table :data="data.bundleContent || []"
+                  border>
+          <el-table-column prop="type"
+                           label="方案类型"
+                           :formatter="formatterGoodType"
+                           width="200px">
+          </el-table-column>
+          <el-table-column prop="bundleName"
+                           label="方案名称">
+          </el-table-column>
+
+        </el-table>
+      </card-table>
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+import CardTable from '@/components/card-table'
+import TableInfo from '@/components/table-info'
+import {price} from '@/const/filter'
+import {productDetail} from '@/const/api'
+import {goodsType} from '@/const/config'
+import zipObject from 'lodash/zipObject'
+import split from 'lodash/split'
+export default {
+  name: 'goods-detail',
+  components: {
+    CardTable,
+    TableInfo
+  },
+  data() {
+    return {
+      data: {},
+      tableData: []
+    }
+  },
+  methods: {
+    formatter(row, column) {
+      return row['type'] === 'base' ? '基础属性' : '销售属性'
+    },
+    formatterGoodType(row, column) {
+      return goodsType[row.type]
+    },
+    price
+  },
+  computed: {
+    isNewCar() {
+      return true
+    },
+    id() {
+      return this.$route.query.productId
+    },
+    skuTableColumn() {
+      const skus = this.data.skus || []
+      if (skus.length === 0) return []
+
+      const {propNames} = skus[0]
+      const result = propNames.split(',').map(item => {
+        return {
+          label: item,
+          prop: item
+        }
+      })
+      return result.concat([
+        {
+          label: '指导价',
+          prop: 'guidePrice'
+        },
+        {
+          label: '优惠价',
+          prop: 'preferentialPrice',
+          type: 'price'
+        }
+      ])
+    },
+    skuTableData() {
+      const result = (this.data.skus || []).map(item => {
+        const {
+          propNames = '',
+          propValues = '',
+          minPrice,
+          maxPrice,
+          preferentialPrice,
+          guidePrice
+        } = item
+        const data = zipObject(split(propNames, ','), split(propValues, ','))
+        data.preferentialPrice = price(preferentialPrice) || 0
+        data.guidePrice = price(guidePrice) || 0
+        data.priceRange = `${minPrice || 0},${maxPrice || 0}`
+
+        return data
+      })
+      return result
+    },
+    catalogName() {
+      return this.data.prdCatalog ? this.data.prdCatalog.name : ''
+    },
+    brandName() {
+      const brandList = this.data.brandList || []
+      const brand = brandList[0] || {}
+      return brand.name || ''
+    },
+    productPhoto() {
+      return split(this.data.productPhoto, ',')
+    }
+  },
+  mounted() {
+    this.$axios.$get(productDetail(this.id)).then(result => {
+      this.data = result.payload
+    })
+  }
+}
+</script>
+
+<style lang="stylus">
+.goods-detail {
+  .car-info {
+    display: flex;
+    width: 100%;
+
+    .img {
+      margin-right: 30px;
+    }
+
+    .table {
+      flex: 1;
+    }
+
+    img {
+      max-width: 120px;
+    }
+  }
+
+  .value {
+    img {
+      width: 100px;
+    }
+  }
+
+  .tab-container {
+    margin-top: 30px;
+    min-height: 300px;
+  }
+}
+</style>
---
+++ src/pages/goods/goods-audit-list.vue
@@ -0,0 +1,185 @@
+<template>
+  <div :class="pageName">
+    <el-data-table
+      totalPath="payload.total"
+      dataPath="payload.list"
+      ref="dataTable"
+      :url="url"
+      :columns="columns"
+      :hasNew="false"
+      :hasEdit="false"
+      :hasDelete="false"
+      :hasOperation="true"
+      :isTree="false"
+      :hasPagination="true"
+      :extraButtons="extraButtons"
+      :searchForm="searchForm"
+      :extraParams="extraParams"
+      :operationAttrs="operationAttrs"
+      :customQuery="customQuery"
+      @reset="handleReset"
+    >
+      <template slot="search">
+        <el-form-item label="后台类目" prop="catalogId">
+          <back-end-category-select @change="handleSelect('catalogId', $event)"
+                                    ref="catalogId"></back-end-category-select>
+        </el-form-item>
+      </template>
+    </el-data-table>
+  </div>
+</template>
+
+<script>
+import {goodsLists, goodsCancelApply} from '@/const/api'
+import {goodsListColumns} from '@/const/goods'
+import {listSearchForm} from '@/const/goods'
+import {getGoodsOnOffStatus, formatDate} from '@/const/filter'
+import {goodsDetail, goodsPublish, goodsUseTplPublish} from '@/const/path'
+import BackEndCategorySelect from '@/container/back-end-category-select/'
+
+// searchForm中slot（门店和后台类目）查询取值和重置都在mixIn中
+import searchFormMixin from '@/mixins/search-form-slot'
+
+const status2String = {
+  passed: '审核通过',
+  unaudited: '待审核',
+  rejected: '审核拒绝',
+  canceled: '已取消申请',
+  draft: '草稿'
+}
+const channels = ['我买网', '京东', '自营']
+export default {
+  name: 'goods-list',
+  components: {
+    BackEndCategorySelect
+  },
+  mixins: [searchFormMixin],
+  data() {
+    let extraParams = {}
+    return {
+      pageName: 'goods-list',
+      url: goodsLists,
+      columns: [
+        {
+          prop: 'code',
+          label: '商品编号',
+          'show-overflow-tooltip': true
+        },
+        {
+          prop: 'name',
+          label: '商品名称',
+          minWidth: '200',
+          'show-overflow-tooltip': true
+        },
+        {
+          prop: 'channel',
+          label: '商品渠道',
+          formatter: row => {
+            return channels[row.channel]
+          }
+        },
+        {
+          prop: 'catalogName',
+          label: '后台类目'
+        },
+        {
+          prop: 'status',
+          label: '状态',
+          formatter: row => status2String[row.status]
+        },
+        {
+          prop: 'shelf',
+          label: '上架/下架',
+          formatter: row => {
+            return row.onCount + '/' + row.offCount
+          }
+        },
+        {
+          prop: 'createdAt',
+          label: '创建时间',
+          minWidth: '150',
+          formatter: row => {
+            return formatDate(row.createdAt, 'YYYY-MM-DD HH:mm:ss')
+          }
+        }
+      ],
+      dialogVisible: false,
+      form: [],
+      extraButtons: [
+        {
+          text: '查看',
+          type: 'primary',
+          atClick: row => {
+            this.$router.push({
+              path: '/goods/goods-detail',
+              query: {
+                // isView: 1,
+                productId: row.itemId
+              }
+            })
+          }
+        }
+      ],
+      searchForm: [
+        {
+          $el: {placeholder: '请输入商品编号'},
+          label: '商品编号',
+          $id: 'code',
+          $type: 'input'
+        },
+        {
+          $el: {placeholder: '请输入商品名称'},
+          label: '商品名称',
+          $id: 'name',
+          $type: 'input'
+        },
+        {
+          $el: {placeholder: '请输入商品渠道'},
+          label: '商品渠道',
+          $id: 'channel',
+          $type: 'select',
+          $options: [
+            {
+              label: '我买网',
+              value: '0'
+            },
+            {
+              label: '京东',
+              value: '1'
+            },
+            {
+              label: '自营',
+              value: '2'
+            }
+          ]
+        }
+      ],
+      operationAttrs: {
+        fixed: 'right'
+      },
+      customQuery: {
+        catalogId: ''
+      },
+      extraParams
+    }
+  },
+  methods: {
+    handleSelect(type, ids) {
+      // if(!ids){
+      //   return
+      // }else{
+      //   if(ids.length===1)
+      //   {return}
+      // }
+      this.customQuery[type] = ids[ids.length - 1]
+    }
+  },
+  async asyncData({app}) {},
+  mounted() {}
+}
+</script>
+<style lang="stylus" scoped>
+  .goods-list {
+
+  }
+</style>
