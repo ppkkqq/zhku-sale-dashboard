a023f73dcc9a24fa2b8c1fee7a6a4c4bd5669513
Edgar <15989069487@163.com>
2018-10-23 14:41:15 +0800

    更换ico透明背景

--- nuxt.config.js
+++ nuxt.config.js
@@ -100,7 +100,7 @@ module.exports = {
         rel: 'icon',
         type: 'image/x-icon',
         href:
-          '//gtytoss.oss-cn-hangzhou.aliyuncs.com/platform/favicon-1540276038073.png'
+          '//gtytoss.oss-cn-hangzhou.aliyuncs.com/platform/favicon-1540276831210.png'
       }
     ]
   },
