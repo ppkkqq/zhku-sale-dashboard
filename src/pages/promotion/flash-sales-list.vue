f2b480a752c4587362de78b3cff1f9d91d7137a7
untaken <292234463@qq.com>
2018-11-13 16:51:55 +0800

    add flash-sales-list

---
+++ src/pages/promotion/flash-sales-list.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div :class="pageName">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'flash-sales-list',
+  components: {},
+  data() {
+    return {
+      pageName: 'flash-sales-list'
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
