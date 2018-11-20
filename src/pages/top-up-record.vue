6c006f67441a076f785fcf7c895c35ddbd635c79
linzehui <381709042@qq.com>
2018-11-20 19:56:25 +0800

    修改源通币充值页面样式

--- src/pages/top-up-record.vue
+++ src/pages/top-up-record.vue
@@ -30,11 +30,6 @@ export default {
       //实际中去掉了用户手机号的列，充值前值和充值后值合并为充值总额。
       columns: [
         {
-          label: '序号',
-          type: 'index',
-          minwidth: 100
-        },
-        {
           prop: 'memberName',
           label: '会员昵称'
         },
@@ -58,7 +53,7 @@ export default {
         {
           prop: 'eventTime',
           label: '充值时间',
-          width: '150px',
+          width: '160px',
           formatter: row => {
             return formatDate(row.eventTime, 'YYYY-MM-DD HH:mm:ss')
           }
@@ -72,7 +67,7 @@ export default {
             placeholder: '请输入会员手机号'
           },
           label: '会员手机号',
-          $id: 'memberPhone',
+          $id: 'mobile',
           $type: 'input'
         },
         {
@@ -80,7 +75,7 @@ export default {
             placeholder: '请输入用户名称'
           },
           label: '用户名称',
-          $id: 'userName',
+          $id: 'fullname',
           $type: 'input'
         }
       ]
