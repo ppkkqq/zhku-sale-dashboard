b96ef9202e0a34e5e03fde7e496df98532170ec0
qiye <lujunwei@deepexi.com>
2018-11-29 16:19:35 +0800

    【会员列表】模拟点击导入按钮

--- src/pages/customer-list.vue
+++ src/pages/customer-list.vue
@@ -95,10 +95,24 @@
         </el-form-item>
       </el-form>
     </el-dialog>
+
+    <el-upload
+      ref="upload"
+      :on-change="importExcel"
+      :action='`${url}`'
+      :multiple='false'
+      :auto-upload="false"
+      :http-request="httpRequest"
+      show-file-list
+      :on-success='onSuccess'
+      :file-list="fileList">
+      <el-button slot="trigger" size="small" type="primary" v-loading='importLoading'></el-button>
+    </el-upload>
   </div>
 </template>
 
 <script>
+import {Upload} from 'element-ui'
 import {formatDate} from '@/const/filter'
 import {
   mcMemberInfos,
@@ -232,7 +246,10 @@ export default {
         {
           text: '导入会员',
           type: 'primary',
-          atClick: () => {}
+          atClick: () => {
+            // 模拟点击导入组件
+            this.$refs.upload.$el.querySelector('[type=file]').click()
+          }
         },
         {
           text: '下载导入模板',
@@ -271,7 +288,6 @@ export default {
           $type: 'input'
         }
       ],
-
       single,
       batch,
       topUpLoading: false,
@@ -281,12 +297,21 @@ export default {
         mobiles: {trigger: 'blur', validator: validateMobile}
       },
       currentDialog: single,
-      topUpDialogVisible: false
+      topUpDialogVisible: false,
+      importLoading: false
     }
+  },
+  components: {
+    'el-upload': Upload
   },
   computed: {
     topUpDialogTitle() {
       return dialogTitle[this.currentDialog]
+    },
+    url() {
+      return (
+        '/pmsX-api/api/v1/admin/manhours/manhourbacktrack?token=' + this.token
+      )
     }
   },
   watch: {
@@ -372,6 +397,152 @@ export default {
       this.customQuery.endLastLoginTime = ''
       this.createDate = []
       this.lastLoginTime = []
+    },
+    // 导入excel，csv格式
+    importExcel(file) {
+      const types = file.name.split('.')[1]
+      this.fileExcel(file).then(tabJson => {
+        if (tabJson && tabJson.length > 0) {
+          this.xlsxJson = tabJson
+        }
+        if (this.xlsxJson.length > 0) {
+          this.submitUpload()
+          this.importLoading = true
+        }
+      })
+    },
+    submitUpload() {
+      this.$refs.upload.submit()
+    },
+    openSuccess() {
+      this.$notify({
+        title: '工时补录',
+        message: '上传成功',
+        type: 'success'
+      })
+    },
+    openFail() {
+      this.$notify.error({
+        title: '工时补录',
+        message: '工时类型应该是加班或正常'
+      })
+    },
+    onSuccess(response, file, fileList) {
+      this.$refs.upload.clearFiles()
+    },
+    transferCouponValueTime(startDate, valueTime) {
+      var date = new Date(startDate)
+      var newDate = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate() - 1 + valueTime
+      )
+      var year1 = date.getFullYear()
+      var month1 = date.getMonth() + 1
+      var day1 = date.getDate()
+      var year2 = newDate.getFullYear() - 70
+      var month2 = newDate.getMonth() + 1
+      var day2 = newDate.getDate()
+      if (day2 > 10) {
+        return year2 + '-' + month2 + '-' + day2
+      } else {
+        return year2 + '-' + month2 + '-' + '0' + day2
+      }
+    },
+    fileExcel(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = e => {
+          let data = e.target.result
+
+          if (!FileReader.prototype.readAsBinaryString) {
+            var str = cptable.utils.decode(936, data) //字符编码转utf-8
+
+            data = new Uint8Array(str)
+          } else {
+            data = cptable.utils.decode(936, data) //字符编码转utf-8
+          }
+          let wb = XLSX.read(data, {
+            type: 'binary'
+          })
+          let result = []
+          let resuleChange
+          wb.SheetNames.forEach(sheetName => {
+            result.push({
+              sheetName: sheetName,
+              // sheet: XLSX.utils.sheet_to_json(wb.Sheets[sheetName])
+              sheet: XLSX.utils.sheet_to_csv(wb.Sheets[sheetName])
+            })
+          })
+          console.log(wb)
+          console.log(result)
+          result.forEach((value, index) => {
+            if (value.sheet.length > 0) {
+              value.sheet.forEach((Ovalue, Oindex) => {
+                // let mapKey = {
+                //   任务内容: 'taskContent',
+                //   工时: 'manHour',
+                //   工时类型: 'hourType',
+                //   漏填时间: 'fillDate',
+                //   用户名: 'name',
+                //   项目名称: 'projectName'
+                // }
+                let mapKey = {
+                  昵称: 'nickName',
+                  姓名: 'name',
+                  手机号: 'mobile',
+                  性别: 'gender',
+                  生日: 'birth',
+                  邮箱: 'email'
+                }
+                resuleChange = Object.keys(Ovalue).reduce((result, key) => {
+                  result[mapKey[key]] = Ovalue[key]
+
+                  return result
+                }, {})
+
+                this.resultArray.push(resuleChange)
+              })
+            }
+          })
+          this.resultArray.forEach((value, index) => {
+            if (value.hourType == '加班') {
+              value.hourType = 1
+              value.fillDate = this.transferCouponValueTime(1, value.fillDate)
+            } else if (value.hourType == '正常') {
+              value.hourType = 0
+              value.fillDate = this.transferCouponValueTime(1, value.fillDate)
+            } else {
+              value.hourType = 2
+              this.resultArray = []
+              this.openFail()
+              this.$refs.upload.clearFiles()
+            }
+          })
+          resolve(this.resultArray)
+        }
+        reader.readAsBinaryString(file.raw)
+      })
+    },
+    httpRequest() {
+      //自定义上传的实现
+      this.$axios
+        .post(this.url, this.resultArray)
+        .then(response => {
+          if (response.data.length > 0) {
+            this.openSuccess()
+            this.$refs.upload.clearFiles()
+            this.resultArray = []
+            this.importLoading = false
+          }
+        })
+        .catch(error => {
+          if (error.response) {
+            this.$refs.upload.clearFiles()
+            this.resultArray = []
+            this.importLoading = false
+          }
+        })
     }
   }
 }
