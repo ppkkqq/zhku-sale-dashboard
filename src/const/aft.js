03dfcf26a912dc8179c1f17aa70d8f31fd01e9a5
untaken <292234463@qq.com>
2018-11-29 16:20:32 +0800

    售后列表 增加搜索条件

--- src/const/aft.js
+++ src/const/aft.js
@@ -1,4 +1,4 @@
-import {toOptionsLabel, formatDate, price} from '@/const/filter'
+import {Object2Options, formatDate, price} from '@/const/filter'
 
 const format = {
   dateTime: (row, column, val) => formatDate(val, 'YYYY-MM-DD HH : mm'),
@@ -6,18 +6,28 @@ const format = {
 }
 
 export const statusOpts = {
+  // 退款
+  REFUNDING: '退款中',
+  CONFIRM_REFUND: '确认退款',
+  CANCEL_REFUND: '取消退款',
+  // 退货
+  RETURNING: '退货中',
+  CONFIRM_RETURN: '确认退货',
+  CANCEL_RETURN: '取消退货退款',
+  // 换货
+  DAISHOUHUO: '待收货',
+  DAIFAHUO: '待发货',
+  CONFIRM_FAHUO: '确认换货',
+  CANCEL: '取消',
+  FAILED_REVIEW: '审核失败',
+
   WAIT_AUDIT: '待审核',
   AUDITED: '已审核',
   REFUNDED: '已退款',
-  REFUNDING: '退款中',
   REJECTED: '已拒绝',
-  UNDER_REVIEW: '审核中',
-  CONFIRM_REFUND: '确认退款',
-  CANCEL: '取消',
-  RETURNING: '退货中',
-  CONFIRM_RETURN: '确认退货',
-  FAILED_REVIEW: '审核失败'
+  UNDER_REVIEW: '审核中'
 }
+
 export const searchForm = [
   {
     $el: {
@@ -39,8 +49,16 @@ export const searchForm = [
     $el: {
       placeholder: '请输入'
     },
-    label: '会员账号',
+    label: '会员昵称',
     $id: 'memberName',
+    $type: 'input'
+  },
+  {
+    $el: {
+      placeholder: '请输入'
+    },
+    label: '会员手机号',
+    $id: 'memberName', //
     $type: 'input'
   },
   {
@@ -70,14 +88,23 @@ export const searchForm = [
     $type: 'select',
     $options: [
       {
-        label: '退款',
-        value: 1
-      },
-      {
         label: '退货',
         value: 3
+      },
+      {
+        label: '退款',
+        value: 1
       }
     ]
+  },
+  {
+    $el: {
+      placeholder: '请输入'
+    },
+    label: '售后申请状态', //
+    $id: 'status', //
+    $type: 'select',
+    $options: Object2Options(statusOpts)
   }
 ]
 export const columns = [
@@ -96,12 +123,13 @@ export const columns = [
   {
     prop: 'source',
     label: '商品来源',
-    minWidth: 180,
+    minWidth: 100,
     showOverflowTooltip: true
   },
   {
     prop: 'memberName',
-    label: '会员账号',
+    label: '会员昵称',
+    minWidth: 100,
     showOverflowTooltip: true
   },
   {
@@ -122,7 +150,8 @@ export const columns = [
   },
   {
     prop: 'status',
-    label: '订单状态'
+    label: '售后申请状态',
+    minWidth: 120
     // formatter: row => statusOpts[row.status]
   }
 ]
